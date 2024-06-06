cbf91402ea5792ddadbb12eea5137c179fe2f0d7
user <user@DESKTOP-HJGO2JE>
2024-06-06 22:29:02 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.sor
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
